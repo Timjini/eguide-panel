dbf1b82b16fe2c04371a57add9abf0fa24d52a10
Timjini <hatim.jini@gmail.com>
2026-01-01 12:18:43 +0000

    production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import tailwindcss from '@tailwindcss/vite'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views /*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans
-                ],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                accentIndigo: "text-indigo-300"
-            }
         },
     },
 
-    plugins: [forms
+    plugins: [
+        forms,
     ],
 };
