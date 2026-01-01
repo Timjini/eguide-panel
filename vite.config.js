04170d7e1358b777e6ce2e0d0ce90d898754de61
Timjini <hatim.jini@gmail.com>
2026-01-01 21:18:28 +0000

    production setup

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
+
 export default defineConfig({
-    plugins: [
+ plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        cors: true,
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+  build: {
+    outDir: 'public/build', 
+    assetsDir: 'assets', 
+  },
+  base: 'public',
 });
